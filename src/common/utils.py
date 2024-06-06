90b35a57b26d5469cefae1ee871d56d445028138
Steven Stewart <stewart.steven98@outlook.com>
2024-06-05 22:13:38 -0400

    cleanup keyvalue

--- src/common/utils.py
+++ src/common/utils.py
@@ -4,7 +4,7 @@ import discord
 def convert_list_to_select_options(items: List[Dict[str, str]]) -> List[discord.SelectOption]:
     options: List[discord.SelectOption] = []
     for item in items:
-        option = discord.SelectOption(label=item.get("action"), value=item.get("description"))
+        option = discord.SelectOption(label=item.get("description"), value=item.get("action"))
         options.append(option)
     return options
 
